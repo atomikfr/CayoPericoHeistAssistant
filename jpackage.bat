12c6a1bec557653139a957fd9a8be46419fe7df0
josephsmendoza <josephsmendoza@gmail.com>
2021-01-05 21:10:18 +0000

    use jpackage

---
+++ jpackage.bat
@@ -0,0 +1,2 @@
+copy target\CayoPericoHeistAssistant.jar CayoPericoHeistAssistant\
+jpackage.exe --input CayoPericoHeistAssistant\ --app-version 0.10 --name CayoPericoHeistAssistant --main-jar CayoPericoHeistAssistant.jar --type msi --win-menu --win-shortcut --win-dir-chooser --win-per-user-install
